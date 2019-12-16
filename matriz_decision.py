b32c18b95e16b60f8f7a4e93379b441b118ba2f3
Alisson <alisson@getty.io>
2019-12-16 00:26:51 -0300

    (feat) matriz beneficio funciones

--- matriz_decision.py
+++ matriz_decision.py
@@ -1,6 +1,28 @@
 
-def matriz_beneficio(demanda, stock):
+def matriz_beneficio(demanda, stock, venta, compra):
     matriz_principal = []
+    print('', stock)
+    for _demanda in stock:
+        matriz_1 = []
+        matriz_respuesta = []
+        for _stock in demanda:
+            resultado = 0
+            if _stock > _demanda:
+                resultado = (venta * _demanda) - (compra * _stock)
+                resultado_str = 'Venta %s * Demanda %s - Compra %s * Stock %s = %s' % (venta, _demanda, compra, _stock, resultado)
+            elif _stock <= _demanda:
+                resultado = (venta * _stock) - (compra * _stock)
+                resultado_str = 'Venta %s * Stock %s - Compra %s * Stock %s = %s' % (venta, _stock, compra, _stock, resultado)
+            
+            print(resultado_str)
+            matriz_1.append(resultado)
+        # print(matriz_respuesta)
+        print(_demanda, matriz_1)
+        matriz_principal.append(matriz_1)
 
 
-matriz_beneficio(5, 5)
+demanda = [0, 1, 2, 3, 4, 5]
+stock = [0, 1, 2, 3, 4, 5]
+venta = 30
+compra = 20
+matriz_beneficio(demanda, stock, venta, compra)
